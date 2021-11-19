380eb8023c77cf90d3895cc2d1906d738466acc6
FebryantoAdityaRizky020204 <febryantoadityarizky@gmail.com>
2021-11-19 21:19:43 +0700

    Laravel: fix some bugs and make register user

---
+++ LKS_PROV/MODULE_SERVER/MEDIA FILES/laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function getUsersList(){
+        $users = User::get();
+        return response()->json(['users' => $users], 200);
+    }
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|min:8',
+            'password' => 'required|min:8'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+            'api_token' => 1
+        ]);
+
+        $user->update(['api_token' => bcrypt($user->id)]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Register Succesfuly'
+        ], 201);
+
+
+    }
+}
