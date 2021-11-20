5e62970ce60d186d9e5ad6a9311f931942635b60
FebryantoAdityaRizky020204 <febryantoadityarizky@gmail.com>
2021-11-20 20:33:40 +0700

    Laravel: Make order Controller, i think it's finished, nad if i miss something i can complete later

--- LKS_PROV/MODULE_SERVER/MEDIA FILES/laravel/app/Http/Controllers/OrderController.php
+++ LKS_PROV/MODULE_SERVER/MEDIA FILES/laravel/app/Http/Controllers/OrderController.php
@@ -7,6 +7,9 @@ use App\Models\Order;
 use App\Models\Driver;
 use App\Models\Bus;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+// use Illuminate\Support\Carbon;
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
@@ -17,7 +20,13 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'All Data on table Orders',
+            'data' => $orders
+        ],200);
     }
 
     /**
@@ -29,7 +38,69 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validator = Validator::make($request->all(), [
+            'contact_name' => 'string|required',
+            'contact_phone' => 'string|required',
+            'start_rent_date' => 'date|after:'.Carbon::now().'|required',
+            'total_rent_days' => 'integer|min:1|required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+        
+        // request date
+        $requestDate = $request->start_rent_date;
+
+        $driverOrders = Order::where('driver_id', $request->driver_id)->get();
+        foreach($driverOrders as $driverOrder){
+            // get the start_rent_date
+            $srd = $driverOrder->start_rent_date;
+            // get total rent days
+            $trd = $driverOrder->total_rent_days - 1;
+            // get the end rent date
+            $erd = date('Y-m-d', strtotime($srd . "+".$trd." days"));
+
+            if($requestDate >= $srd && $requestDate <= $erd){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'conflicting order or no available bus or driver',
+                ],401);
+            }
+        }
+
+        $busOrders = Order::where('bus_id', $request->bus_id)->get();
+        foreach($busOrders as $busDriver){
+            // get the start_rent_date
+            $srd = $busDriver->start_rent_date;
+            // get total rent days
+            $trd = $busDriver->total_rent_days -1;
+            // get the end rent date
+            $erd = date('Y-m-d', strtotime($srd . "+".$trd." days"));
+
+            if($requestDate >= $srd && $requestDate <= $erd){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'conflicting order or no available bus or driver',
+                ],401);
+            }
+        }
+
+        $order = Order::create($request->all());
+        if(!empty($order)){
+            return response()->json([
+                'status' => true,
+                'message' => 'order succes',
+                'data' => [
+                    'order' => $order,
+                    'bus' => Bus::find($request->bus_id),
+                    'driver' => Driver::find($request->driver_id),
+                ],
+            ],200);
+        }
+
+        return response()->json();
     }
 
     /**
@@ -40,7 +111,20 @@ class OrderController extends Controller
      */
     public function show($id)
     {
-        //
+        $order = Order::find($id);
+
+        if(!empty($order)){
+            return response()->json([
+                'status' => true,
+                'message' => 'get data with current id succes',
+                'data' => $order
+            ],200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'data with current id not found'
+        ],402);
     }
 
     /**
@@ -63,7 +147,20 @@ class OrderController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $order = Order::find($id);
+
+        if(!empty($order)){
+            $order->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'delete data with current id succes'
+            ],200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'data with current id not found'
+        ],402);
     }
 
 
@@ -73,14 +170,26 @@ class OrderController extends Controller
      */
 
     public function test(Request $request){
-        
+
+        $date = $request->date_try;
+        $customDate = '';
+
+        // first validation
+        $validator = Validator::make($request->all(), [
+            'date_try' => 'required|date'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
 
         return response()->json([
             'status' => true,
             'message' => 'its already run as well',
-            // 'data' => [
-            //     ''
-            // ],
+            'data' => [
+                'your_request' => $request->all(),
+                'custom_date' => $customDate,
+            ],
         ], 200);
     }
 }
